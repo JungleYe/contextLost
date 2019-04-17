36b6f35d907cece8eceb552d226d79024d1a06ca
JungleYe <395321108@qq.com>
2019-04-17 17:06:17 +0800

    Create bar.go

---
+++ bar/bar.go
@@ -0,0 +1,18 @@
+package bar
+
+import "fmt"
+
+type ContextKey struct{}
+var CK = ContextKey{}
+
+type Bar struct {
+	Name string
+}
+
+type Bif interface{
+	PrintB()
+}
+
+func (b *Bar)PrintB(){
+	fmt.Println("b.Name = ",b.Name)
+}
