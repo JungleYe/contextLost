54bc1469d16861e3e36bb768f8ddfffcd89400a1
JungleYe <395321108@qq.com>
2019-04-17 16:59:22 +0800

    the main file

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"glgo/testcode/ctx/bar"
+	"context"
+	"fmt"
+)
+
+func main(){
+	b := new(bar.Bar)
+	b.Name = "world"
+	ctx := newContextWithValue(context.TODO(),bar.CK,b)
+	Pit(ctx)
+}
+
+//通过context来解析
+func Pit(ctx context.Context){
+	//v := ctx.Value(foo.CK)
+	//re,ok := v.(foo.Bif)
+	v := ctx.Value(bar.CK)
+	re,ok := v.(bar.Bif)
+	if ok{
+		re.PrintB()
+	}
+	fmt.Println(v)
+}
+
+//创建一个新的context
+func newContextWithValue(ctx context.Context,ck bar.ContextKey,bif bar.Bif)context.Context{
+	return context.WithValue(context.TODO(),bar.CK,bif)
+}
